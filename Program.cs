6c307e1cb95f10f42e942335287b9d73affee295
Marcin Gomulak <makipol@gmail.com>
2017-07-26 09:48:54 +0200

    Add files via upload
    Fixed parameters

--- Program.cs
+++ Program.cs
@@ -18,13 +18,13 @@ namespace TIMpal_to_JASC
                 System.IO.File.WriteAllLines(@"D:/window.txt", retme);
                 Console.WriteLine("Converted!");
             }
-            if(args.Length !=3)
+            if(args.Length !=2)
             {
                 Console.WriteLine("TIM to Paint Shop Pro");
                 Console.WriteLine("Usage:\nTIMpal_PSP.exe file.tim out.psp\nInput: file.tim - specify 8 BPP or 4 BPP TIM file\tOutput file: out.txt specify output file\n");
                 return;
             }
-            if(args.Length == 3)
+            if(args.Length == 2)
             {
                 TIM tim = new TIM(args[1]);
                 string[] retme = tim.ConvertToPS;
